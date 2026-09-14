2330f5b108d63ebae216de330fc4e46051523f6b
4dblackhole <4dblackhole@naver.com>
2026-09-15 07:30:42 +0900

    Move EditorScene into top-level folder

--- Client/App/FingerDrumGame.cpp
+++ Client/App/FingerDrumGame.cpp
@@ -2,7 +2,7 @@
 
 #include "App/AssetPaths.h"
 #include "GameFlow/FingerDrumSceneIds.h"
-#include "GameScene/EditorScene.h"
+#include "EditorScene/EditorScene.h"
 #include "GameScene/FingerDrumLogoScene.h"
 #include "GameScene/MusicSelectScene.h"
 #include "GameScene/RhythmTestScene.h"
